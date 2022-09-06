0d27e93acd3f3ed8ba274dcf125842ef053686ca
Adam Park <parksm2@uci.edu>
2022-09-06 17:47:17 +0900

    Crawl data

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request, jsonify
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import requests
 import uuid
 
 app = Flask(__name__)
 
-from pymongo import MongoClient
+
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.o2cbi29.mongodb.net/Cluster0?retryWrites=true&w=majority',
                      UuidRepresentation="standard")
 db = client.dbsparta
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://ticket.interpark.com/ConcertIndex.asp?utm_source=google&utm_medium=cpc&utm_campaign=ticket_concert_20210617_pc_cpc_paidsearch&utm_content=consider_34&utm_term=%EC%BD%98%EC%84%9C%ED%8A%B8&_emk_keyword=%EC%BD%98%EC%84%9C%ED%8A%B8&gclid=Cj0KCQjw39uYBhCLARIsAD_SzMTiWL3pU729Lc47JPjt5zcOhhaMEvhKuSwlt3S4bxOqvXqfSh1Aw2EaAnfwEALw_wcB', headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+title = soup.select('dt.issue_obj');
+
+
+for titles in title:
+    a = titles.select_one('p> span.txt1')
+    print(a.text)
+
 
 
 @app.route('/')
