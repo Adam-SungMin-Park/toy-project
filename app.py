d334471be43b199aa555dba1edf840a751b22d79
Adam Park <parksm2@uci.edu>
2022-09-08 17:11:05 +0900

    selenium webdriver

--- app.py
+++ app.py
@@ -1,80 +1,90 @@
 from flask import Flask, render_template, request, jsonify
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import requests
 import uuid
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://ticket.interpark.com/ConcertIndex.asp?utm_source=google&utm_medium=cpc&utm_campaign=ticket_concert_20210617_pc_cpc_paidsearch&utm_content=consider_34&utm_term=%EC%BD%98%EC%84%9C%ED%8A%B8&_emk_keyword=%EC%BD%98%EC%84%9C%ED%8A%B8&gclid=Cj0KCQjw39uYBhCLARIsAD_SzMTiWL3pU729Lc47JPjt5zcOhhaMEvhKuSwlt3S4bxOqvXqfSh1Aw2EaAnfwEALw_wcB')
+testing = driver.find_elements(By.CLASS_NAME, 'issue_obj')
+print(testing[0])
+
+
 app = Flask(__name__)
 
-def id_generator():
-    return str(uuid.uuid4().int)[30:]
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.o2cbi29.mongodb.net/Cluster0?retryWrites=true&w=majority',
-                     UuidRepresentation="standard")
-db = client.dbsparta
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://ticket.interpark.com/ConcertIndex.asp?utm_source=google&utm_medium=cpc&utm_campaign=ticket_concert_20210617_pc_cpc_paidsearch&utm_content=consider_34&utm_term=%EC%BD%98%EC%84%9C%ED%8A%B8&_emk_keyword=%EC%BD%98%EC%84%9C%ED%8A%B8&gclid=Cj0KCQjw39uYBhCLARIsAD_SzMTiWL3pU729Lc47JPjt5zcOhhaMEvhKuSwlt3S4bxOqvXqfSh1Aw2EaAnfwEALw_wcB', headers=headers)
-
-
-soup = BeautifulSoup(data.text, 'html.parser')
-
-
-
-
-title = soup.select('dt.issue_obj');
-for titles in title:
-    singer = titles.select_one('p> span.txt1').text
-    location = titles.select_one('p> span.txt2').text
-    image = titles.select_one('div.thumb> img ')['src']
-    doc = {
-        'id': id_generator(),
-        'singer': singer,
-        'location': location,
-        'image': image
-    }
-    # detail_serial = image[66:74]
-    # for numbers in detail_serial:
-    #     detail_data = requests.get('https://tickets.interpark.com/goods/'+ numbers)
-    #     second_soup = BeautifulSoup(detail_data.text, 'html.parser')
-    #     location = second_soup.select_one('#container > div.contents > div.productWrapper > div.productMain > div.productMainTop > div > div.summaryBody > ul > li:nth-child(2) > div > p')
-    #     print(location)
-    # STOPPED HERE. TRY USING SELENIUM TO RETRIEVE JS DATA 
-
-    if len(list(db.toyproject.find({}))) < 40:
-        db.toyproject.insert_one(doc)
-
-
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/detail')
-def more_info():
-    return render_template('moreInfo.html')
-#the detail page url = "https://tickets.interpark.com/goods/"+ last 7 digits
-
-
-
-
-@app.route('/popup' , methods =['GET'])
-def get_popup_info():
-    url_receive = request.form('url_give')
-    detail_data = requests.get('https://tickets.interpark.com/goods/'+url_receive, headers=headers)
-    return detail_data
-
-
-@app.route('/home', methods = ['GET'])
-def load_homepage():
-    allList = list(db.toyproject.find({},{'_id':False}))
-    return jsonify({'msg':allList})
-
-
-
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+# def id_generator():
+#     return str(uuid.uuid4().int)[30:]
+#
+# client = MongoClient('mongodb+srv://test:sparta@cluster0.o2cbi29.mongodb.net/Cluster0?retryWrites=true&w=majority',
+#                      UuidRepresentation="standard")
+# db = client.dbsparta
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get('https://ticket.interpark.com/ConcertIndex.asp?utm_source=google&utm_medium=cpc&utm_campaign=ticket_concert_20210617_pc_cpc_paidsearch&utm_content=consider_34&utm_term=%EC%BD%98%EC%84%9C%ED%8A%B8&_emk_keyword=%EC%BD%98%EC%84%9C%ED%8A%B8&gclid=Cj0KCQjw39uYBhCLARIsAD_SzMTiWL3pU729Lc47JPjt5zcOhhaMEvhKuSwlt3S4bxOqvXqfSh1Aw2EaAnfwEALw_wcB', headers=headers)
+#
+#
+# soup = BeautifulSoup(data.text, 'html.parser')
+#
+#
+#
+#
+# title = soup.select('dt.issue_obj');
+# for titles in title:
+#     singer = titles.select_one('p> span.txt1').text
+#     location = titles.select_one('p> span.txt2').text
+#     image = titles.select_one('div.thumb> img ')['src']
+#     doc = {
+#         'id': id_generator(),
+#         'singer': singer,
+#         'location': location,
+#         'image': image
+#     }
+#     # detail_serial = image[66:74]
+#     # for numbers in detail_serial:
+#     #     detail_data = requests.get('https://tickets.interpark.com/goods/'+ numbers)
+#     #     second_soup = BeautifulSoup(detail_data.text, 'html.parser')
+#     #     location = second_soup.select_one('#container > div.contents > div.productWrapper > div.productMain > div.productMainTop > div > div.summaryBody > ul > li:nth-child(2) > div > p')
+#     #     print(location)
+#     # STOPPED HERE. TRY USING SELENIUM TO RETRIEVE JS DATA
+#
+#     if len(list(db.toyproject.find({}))) < 40:
+#         db.toyproject.insert_one(doc)
+#
+#
+#
+#
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+#
+#
+# @app.route('/detail')
+# def more_info():
+#     return render_template('moreInfo.html')
+# #the detail page url = "https://tickets.interpark.com/goods/"+ last 7 digits
+#
+#
+#
+#
+# @app.route('/popup' , methods =['GET'])
+# def get_popup_info():
+#     url_receive = request.form('url_give')
+#     detail_data = requests.get('https://tickets.interpark.com/goods/'+url_receive, headers=headers)
+#     return detail_data
+#
+#
+# @app.route('/home', methods = ['GET'])
+# def load_homepage():
+#     allList = list(db.toyproject.find({},{'_id':False}))
+#     return jsonify({'msg':allList})
+#
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run('0.0.0.0', port=5000, debug=True)
