acf2a0be57460f5f6e38adca70c0a94c74d2488e
Adam Park <parksm2@uci.edu>
2022-09-12 19:00:25 +0900

    dd

--- db.py
+++ db.py
@@ -1,1 +1,4 @@
-dummy value
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.o2cbi29.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     UuidRepresentation="standard")
