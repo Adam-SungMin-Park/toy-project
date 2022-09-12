6a692ea7e7940e498fb4d31b47bd34bc39e36c2e
Adam Park <parksm2@uci.edu>
2022-09-12 19:10:02 +0900

    encrypt stage

--- db.py
+++ db.py
@@ -2,3 +2,5 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.o2cbi29.mongodb.net/Cluster0?retryWrites=true&w=majority',
                      UuidRepresentation="standard")
+#
+# 
